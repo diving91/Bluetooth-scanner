e298e96f72e74475edb802c1d7fff0e4bfa0f524
Diving91 <avhs@free.fr>
2017-04-29 01:10:27 +0200

    Add BLE devices support

--- BTdaemon.php
+++ BTdaemon.php
@@ -58,7 +58,7 @@ class BTScanner {
 
 	private $_hcitool;		// Path to hcitool
 	
-	private $_loopTime = 5;		// BT scan loop time
+	private $_loopTime = 2;		// BT scan loop time
 	private $_timeOut = 300;	// Time is seconds before a tag is considered as absent - Use large value to avoid false absence detection
 	private $_debug;		// For debug purpose - Settled at Class construct time
 
